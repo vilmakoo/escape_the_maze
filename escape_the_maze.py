f9a28b1a6999136b06c951eb6e3cda85b523dbad
vilmakoo <vilma.l.kangas@gmail.com>
2022-03-27 16:33:28 +0300

    Refactor code

--- escape_the_maze.py
+++ escape_the_maze.py
@@ -1,9 +1,9 @@
-"""Algorithm for ecaping a given maze"""
+"""Algorithm for escaping a given maze"""
 
 import sys
 
-def starting_point():
-    """Get starting point"""
+def starting_node():
+    """Get starting node"""
     for i, row in enumerate(maze):
         for j, char in enumerate(row):
             if char == "^":
@@ -13,9 +13,11 @@ def starting_point():
 
 
 def get_neighbours(node):
+    """Get adjacent nodes to a given node"""
     neighbours = []
 
     def in_maze(node):
+        """Check that node is inside the maze and not a block"""
         if node[0] in range(len(maze)) and node[1] in range(len(maze[0])):
             if maze[node[0]][node[1]] != "#":
                 return True
@@ -29,25 +31,30 @@ def get_neighbours(node):
     return list(filter(in_maze, neighbours))
 
 
-visited = []
-queue = []
+def find_path(max_steps):
+    """Find a path out of the maze using breadth first search"""
 
-def escape(visited, starting_point, max_steps):
-    visited.append(starting_point)
-    queue.append([starting_point])
+    visited = []
+    queue = []
+    start_node = starting_node()
+
+    visited.append(start_node)
+    queue.append([start_node])
 
     while queue:
-        path = queue.pop(0)
-        node = path[-1]
+        path = queue.pop(0) # path until this node
+        node = path[-1] # node that is going to be examined now
 
         if len(path) > max_steps:
-            print("No possible route shorter than given length available")
+            print("No route shorter than given length available")
             break
 
         if maze[node[0]][node[1]] == "E":
+            # exit found
             print(path)
             break
 
+        # add neighbours to queue if they're not yet visited
         for neighbour in get_neighbours(node):
             if neighbour not in visited:
                 visited.append(neighbour)
@@ -56,13 +63,6 @@ def escape(visited, starting_point, max_steps):
                 queue.append(new_path)
 
 
-
 if __name__ == "__main__":
     maze = list(map(list, open(sys.argv[1]).read().split("\n")))
-    escape(visited, starting_point(), int(sys.argv[2]))
-
-
-
-# TODO:
-# refaktoroi
-# kommentoi
+    find_path(int(sys.argv[2]))
